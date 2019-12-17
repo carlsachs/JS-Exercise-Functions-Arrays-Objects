0ee83215fbd864d0d570fa34cf16e8710f02896d
carlsachs <sachscarl@gmail.com>
2019-12-16 21:46:14 -0500

    Anotha One

--- index.js
+++ index.js
@@ -217,8 +217,12 @@ function sortCarInventory(arr) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(arr) {
+  let year = [];
+  for(let i=0; i < arr.length; i++){
+    year[i] = arr[i].car_year;
+  }
+  return (year);
 }
 
 /**
